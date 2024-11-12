042f936c400e3559970d2d44b61b81d53f96469c
Pedro Vilela Pereira <152188605+PedroPeraz@users.noreply.github.com>
2024-11-11 22:46:36 -0300

    Alterando arquivos do desafio e evidências

--- Sprint4/desafio/hash/hashscript.py
+++ Sprint4/desafio/hash/hashscript.py
@@ -1,8 +1,19 @@
 import hashlib
 
-string = input("Insira o texto que gostaria de codificar: ")
+continuar = True
 
-string_codificada = hashlib.sha1(string.encode()).hexdigest()
+while continuar == True:
+    print("[CODIFICADOR]")
+    print("[1] Codificar")
+    print("[0] Sair")
+    
+    acao = int(input("Insira o número correspondente a ação desejada: "))
+    if acao == 1:
+        string = input("\nInsira o texto que gostaria de codificar: ")
 
-print(f'O texto "{string}" em hash é igual a: \n{string_codificada}')
+        string_codificada = hashlib.sha1(string.encode()).hexdigest()
+
+        print(f'O texto "{string}" em hash é igual a: \n{string_codificada}\n')
+    if acao == 0:
+        continuar = False
 
